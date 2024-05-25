845d2b03de0bbe06b3661b88bb0216407fd01801
Furover <133924982+Furover@users.noreply.github.com>
2024-05-25 02:58:27 +0000

    Update pyzzatime.py

--- tasks/pyzzatime.py
+++ tasks/pyzzatime.py
@@ -53,7 +53,7 @@ while True:
             print("Até mais! Divirta-se sem ter uma pizza quentinha gostosa e boa com queijo caindo derretido! Já que você não quer uma pizza e ME ODEIA, divirta-se vai, nem ligo.")
             time.sleep(2)
             os.system("clear")
-            print("Seu ip: 192.168.15.30")
+            print("Seu ip: 192.192.19.19")
             print("Enderoço: Rua Vila dos bobões, Curitiba, Brasil")
             print("Nome real: Edevaldo")
             print("Quantidade de pizzas GOSTOSAS: 0")
